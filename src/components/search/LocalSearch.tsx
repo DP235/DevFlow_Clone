03fc4fe86ce5dab188f1772843699df6e5d81bb0
DP235 <phungocphong@gmail.com>
2025-08-07 16:00:08 +0700

    Display tags

--- src/components/search/LocalSearch.tsx
+++ src/components/search/LocalSearch.tsx
@@ -11,13 +11,15 @@ interface Props {
     imgSrc: string;
     placeholder: string;
     otherClasses?: string;
+    iconPosition?: 'left' | 'right'
 }
 
 const LocalSearch = ({
     route,
     imgSrc,
     placeholder,
-    otherClasses
+    otherClasses,
+    iconPosition = 'left'
 }: Props) => {
     const pathname = usePathname();
     const router = useRouter();
@@ -55,13 +57,13 @@ const LocalSearch = ({
 
     return (
         <div className={`background-light800_darkgradient flex min-h-[56px] grow items-center gap-4 rounded-[10px] px-4 ${otherClasses}`}>
-            <Image 
+            {iconPosition === 'left' && <Image 
                 src={imgSrc}
                 width={24}
                 height={24}
                 alt='Search'
                 className='cursor-pointer'
-            />
+            />}
             
             <Input 
                 type='text'
@@ -70,6 +72,13 @@ const LocalSearch = ({
                 onChange={e=> setSearchQuery(e.target.value)}
                 className='paragraph-regular no-focus placeholder text-dark400_light700 border-none shadow-none outline-none'
             />
+                {iconPosition === 'right' && <Image 
+                src={imgSrc}
+                width={15}
+                height={15}
+                alt='Search'
+                className='cursor-pointer'
+            />}
         </div>
     )
 }
